a2aa4f0108ef42fcf3f55da2f0a417725d36788f
Vladimir <vultar1500@gmail.com>
2021-07-05 13:51:29 +0300

    remove recieve and transmit

--- npu/npu_vlan_sdn/src/npu.c
+++ npu/npu_vlan_sdn/src/npu.c
@@ -8,41 +8,30 @@
 #include "table.h"
 
 void process_model(uint8_t in_port_nr,
-				   struct io_fn *io)
+                   struct io_fn *io, 
+                   struct packet_context *prs_ctx,
+                   struct stage_fn *sfn)
 {
-	struct packet_context prs_ctx;
-	struct packet_context lkp_ctx;
-	struct packet_context rsv_ctx;
-	struct replicator_context repl_ctx;
+    struct packet_context lkp_ctx;
+    struct packet_context rsv_ctx;
+    struct replicator_context repl_ctx;
 
-	struct resolve_result rr;
+    struct resolve_result rr;
 
-	struct stage_fn sfn = {
-		.alloc_fb = shbm_alloc_frame_buf,
-		.mmap_fb = shbm_frame_buf_mmap,
-		.free_fb = shbm_free_frame_buf,
-		.read_fb = shbm_read_frame_buf
-	};
+    struct lookup_fn lfn = {
+        .search_rd = lookup_switch
+    };
 
-	struct lookup_fn lfn = {
-	    .search_rd = lookup_switch
-	};
-
-	if (receive(NULL, in_port_nr, &prs_ctx, io, &sfn))
-		return;
-
-	if (parse(NULL, &sfn, &prs_ctx, &lkp_ctx))
-		return;
-
-    rr = match_action_src(NULL, &sfn, &lfn, &lkp_ctx, &rsv_ctx, &repl_ctx);
-    if (rr.out_port >= 0) {
-    	replicator(NULL, &sfn, &repl_ctx);
-    	transmit(io);
-    }
-
-	if (match_action_dst(NULL, &sfn, &lfn, &lkp_ctx, &rsv_ctx, &repl_ctx).drop)
+    if (parse(NULL, sfn, prs_ctx, &lkp_ctx))
         return;
 
-    replicator(NULL, &sfn, &repl_ctx);
-	transmit(io);
+    rr = match_action_src(NULL, sfn, &lfn, &lkp_ctx, &rsv_ctx, &repl_ctx);
+    if (rr.out_port >= 0) {
+        replicator(NULL, sfn, &repl_ctx);
+    }
+
+    if (match_action_dst(NULL, sfn, &lfn, &lkp_ctx, &rsv_ctx, &repl_ctx).drop)
+        return;
+
+    replicator(NULL, sfn, &repl_ctx);
 }
