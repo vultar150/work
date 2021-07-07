91eadb40ae37d26b64b81a2760546fc8cadc8b91
Vladimir <vultar1500@gmail.com>
2021-07-07 14:52:58 +0300

    change search_rd function on lookup_flow

--- npu/npu_vlan_sdn/src/npu.c
+++ npu/npu_vlan_sdn/src/npu.c
@@ -7,6 +7,7 @@
 #include "stages.h"
 #include "table.h"
 
+
 void process_model(uint8_t in_port_nr, 
                    struct packet_context *prs_ctx,
                    struct stage_fn *sfn)
@@ -18,19 +19,21 @@ void process_model(uint8_t in_port_nr,
     struct resolve_result rr;
 
     struct lookup_fn lfn = {
-        .search_rd = lookup_switch
+        .search_rd = lookup_flow
     };
 
-    if (parse(sfn, prs_ctx, &lkp_ctx))
+    if (parse(sfn, prs_ctx, &lkp_ctx)) {
         return;
+    }
 
     rr = match_action_src(sfn, &lfn, &lkp_ctx, &repl_ctx, &rsv_ctx);
     if (rr.out_port >= 0) {
         replicator(sfn, &repl_ctx);
     }
 
-    if (match_action_dst(sfn, &lfn, &rsv_ctx, &repl_ctx, &lkp_ctx).drop)
+    if (match_action_dst(sfn, &lfn, &rsv_ctx, &repl_ctx, &lkp_ctx).drop) {
         return;
+    }
 
     replicator(sfn, &repl_ctx);
 }
