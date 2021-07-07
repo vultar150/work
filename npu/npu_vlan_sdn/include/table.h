5bc4e0c0a92e317c9870e0c4ed73034f7d74b1d2
Vladimir <vultar1500@gmail.com>
2021-07-07 14:44:27 +0300

    add flow and group tables

--- npu/npu_vlan_sdn/include/table.h
+++ npu/npu_vlan_sdn/include/table.h
@@ -2,66 +2,67 @@
 #define NPU_VLAN_SDN_TABLE_H
 
 #include "lib/include/npu.h"
+#include "lib/include/action.h"
 
-#define VLAN_TBL_CAPACITY 4097
+typedef enum {
+    ALL = 0,
+    INDIRECT
+} group_t;
 
-struct vlan_node_item {
-    uint8_t port;
-    tag_mode_t mode;
-};
-
-struct vlan_node {
-    uint8_t len;
-    struct vlan_node_item items[ETH_PORTS_NR];
-};
-
-struct vlan_table {
-    struct vlan_node nodes[VLAN_TBL_CAPACITY];
-};
-
-struct hashnode {
-    struct hashnode *next;
-    uint32_t cc;
+typedef struct flow_entry {
     struct ethaddr mac;
-    uint8_t port;
-    uint16_t vlan;
+    uint16_t vlan_tag;
+    uint32_t cc;
+    instr_t actions[MAX_NM_ACTIONS]; /* list of actions */
+    uint16_t act_len;
+    uint8_t valid;
+} flow_entry_t;
+
+typedef struct action_bucket {
+    instr_t actions[MAX_NM_BUCKET_ACTIONS];
+    uint16_t act_len;
+} action_bucket_t;
+
+typedef struct group_entry {
+    uint16_t group_id;
+    group_t type;
+    uint16_t cc;
+    action_bucket_t buckets[MAX_NM_BUCKETS];
+    uint8_t buck_len;
+} group_entry_t;
+
+struct flow_table {
+    flow_entry_t nodes[MAX_FLOW_TABLE_SIZE];
+    uint16_t len;
 };
 
-struct hashtable {
-    struct hashnode *nodes[HASHNODE_CAPACITY];
-    uint32_t next_node;
+struct group_table {
+    group_entry_t nodes[MAX_GROUPS];
+    uint16_t len;
 };
 
 enum tables {
     SRC_TABLE,
     DST_TABLE,
-    VLAN_TABLE,
+    GROUP_TABLE
 };
 
 struct key {
-    uint32_t hash;
     struct ethaddr mac;
     uint16_t tag;
-
     uint8_t src_port;
     uint8_t dst_port;
 };
 
 struct res {
-    uint8_t len;
-    struct vlan_node_item items[ETH_PORTS_NR - 1];
+    instr_t actions[MAX_NM_ACTIONS];
+    uint16_t len;
 };
 
-struct search_result lookup_switch(struct search_key);
+struct search_result lookup_flow(struct search_key);
 
-void * src_table_mmap(void);
-void * dst_table_mmap(void);
-void * vlan_table_mmap(void);
-
-struct hashnode * get_head(struct hashtable *, uint32_t);
-struct hashnode * get_next(struct hashtable *, uint32_t,
-                           struct hashnode *);
-struct hashnode ** get_head_p(struct hashtable *tbl, uint32_t hash);
-struct hashnode ** get_next_p(struct hashnode **pnode);
+void * src_flow_table_mmap(void);
+void * dst_flow_table_mmap(void);
+void * group_table_mmap(void);
 
 #endif //NPU_VLAN_SDN_TABLE_H
