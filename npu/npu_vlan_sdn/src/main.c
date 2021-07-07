3d227416c82d4f6bcd5ba4bf3f49f4a9597264fa
Vladimir <vultar1500@gmail.com>
2021-07-07 14:52:08 +0300

    change tabs on spaces

--- npu/npu_vlan_sdn/src/main.c
+++ npu/npu_vlan_sdn/src/main.c
@@ -5,17 +5,17 @@
 
 int main(int argc, char **argv)
 {
-	int (*tests[])(struct output_frame_context *,
-				   struct output_frame_context *) = {
-		test_broadcast,
-		test_vlan,
-		test_learn,
-		test_drop
-	};
+    int (*tests[])(struct output_frame_context *,
+                   struct output_frame_context *) = {
+        test_broadcast,
+        test_vlan,
+        test_learn,
+        test_drop
+    };
 
-	for (int i = 0; i < ARRAY_SIZE(tests); i++) {
-		start_test(tests[i], i + 1);
-	}
+    for (int i = 0; i < ARRAY_SIZE(tests); i++) {
+        start_test(tests[i], i + 1);
+    }
 
-	return 0;
+    return 0;
 }
